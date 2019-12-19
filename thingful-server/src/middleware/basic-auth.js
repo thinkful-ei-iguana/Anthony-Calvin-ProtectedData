ddcc9f5b6aa592ad931b2c776be56e9037fe78a0
AnthonyTB <Anthonylol@protonmail.com>
2019-12-19 09:43:25 -0800

    added logout button functionality, seeded the hashed pws into the database, and then updated middleware to hash users login passwords

--- thingful-server/src/middleware/basic-auth.js
+++ thingful-server/src/middleware/basic-auth.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const AuthService = require("./AuthService");
+
 function requireAuth(req, res, next) {
   const authToken = req.get("Authorization") || "";
 
@@ -8,7 +11,7 @@ function requireAuth(req, res, next) {
     basicToken = authToken.slice("basic ".length, authToken.length);
   }
 
-  const [tokenUserName, tokenPassword] = Buffer.from(basicTOken, "base64")
+  const [tokenUserName, tokenPassword] = Buffer.from(basicToken, "base64")
     .toString()
     .split(":");
 
@@ -16,16 +19,21 @@ function requireAuth(req, res, next) {
     return res.status(401).json({ error: "Unauthorized request" });
   }
 
-  req.app
-    .get("db")("thingful_users")
-    .where({ user_name: tokenUserName })
-    .first()
+  AuthService.getUserWithUserName(req.app.get("db"), tokenUserName)
     .then(user => {
       if (!user) {
         return res.status(401).json({ error: "Unauthorized request" });
       }
-      req.user = user;
-      next();
+      return bcrypt
+        .compare(tokenPassword, user.password)
+        .then(passwordsMatch => {
+          if (!passwordsMatch) {
+            return res.status(401).json({ error: "Unauthorized request" });
+          }
+
+          req.user = user;
+          next();
+        });
     })
     .catch(next);
 }
